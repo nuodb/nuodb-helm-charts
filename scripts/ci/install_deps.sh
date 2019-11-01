080ea3ebbebffede9ab1b4c58e89f4fcb06ff760
Martin Kysel <mkysel@nuodb.com>
2019-11-01 15:49:21 -0400

    missing helm

--- scripts/ci/install_deps.sh
+++ scripts/ci/install_deps.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+# Download kubectl, which is a requirement for using minikube.
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v"${KUBERNETES_VERSION}"/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+
+# Download Helm and Tiller
+wget http://storage.googleapis.com/kubernetes-helm/helm-"${HELM_VERSION}"-linux-amd64.tar.gz -O /tmp/helm.tar.gz
+tar xzf /tmp/helm.tar.gz -C /tmp --strip-components=1 && chmod +x /tmp/helm && sudo mv /tmp/helm /usr/local/bin
+
 if [[ -z "$REQUIRES_MINIKUBE" ]]; then
-    echo "skipping installation step"
+    echo "Skipping minikube installation step"
     exit 0
 fi
 
-# Download kubectl, which is a requirement for using minikube.
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v"${KUBERNETES_VERSION}"/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 # Download minikube.
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v"${MINIKUBE_VERSION}"/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 mkdir -p "$HOME"/.kube "$HOME"/.minikube
@@ -17,9 +22,7 @@ sudo minikube start --vm-driver=none --kubernetes-version=v"${KUBERNETES_VERSION
 sudo chown -R travis: /home/travis/.minikube/
 kubectl cluster-info
 
-# install Helm and Tiller
-wget http://storage.googleapis.com/kubernetes-helm/helm-"${HELM_VERSION}"-linux-amd64.tar.gz -O /tmp/helm.tar.gz
-tar xzf /tmp/helm.tar.gz -C /tmp --strip-components=1 && chmod +x /tmp/helm && sudo mv /tmp/helm /usr/local/bin
+# install helm
 helm init
 
 # get the image to speed up tests
