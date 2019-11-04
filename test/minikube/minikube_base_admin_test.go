014c78ecdd81d96f1fb03938cb5bc1978a0bba7a
Martin Kysel <mkysel@nuodb.com>
2019-11-04 09:53:18 -0500

    Rework Travis (#16)
    add caching (saves around 2 min per build)
    split tests into basic/short/long and parallelize builds (saves around 15 min)
    annotate minikube tests with short or long. When building locally I recommend setting both.

--- test/minikube/minikube_base_admin_test.go
+++ test/minikube/minikube_base_admin_test.go
@@ -1,3 +1,5 @@
+// +build short
+
 package minikube
 
 import (
@@ -7,47 +9,10 @@ import (
 	"time"
 
 	"github.com/nuodb/nuodb-helm-charts/test/testlib"
-
-	"gotest.tools/assert"
-
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
 )
-
-func verifyLoadBalancer(t *testing.T, namespaceName string, balancerName string) {
-	kubectlOptions := k8s.NewKubectlOptions("", "")
-	kubectlOptions.Namespace = namespaceName
-
-	balancerService := k8s.GetService(t, kubectlOptions, balancerName)
-	assert.Equal(t, balancerService.Name, balancerName)
-}
-
-func verifyLBPolicy(t *testing.T, namespaceName string, podName string) {
-	testlib.AwaitBalancerTerminated(t, namespaceName, "job-lb-policy")
-	testlib.VerifyPolicyInstalled(t, namespaceName, podName)
-}
-
-func verifyPodKill(t *testing.T, namespaceName string, podName string, helmChartReleaseName string, nrReplicasExpected int) {
-	testlib.KillAdminPod(t, namespaceName, podName)
-	testlib.AwaitNrReplicasScheduled(t, namespaceName, helmChartReleaseName, nrReplicasExpected)
-	testlib.AwaitAdminPodUp(t, namespaceName, podName, 100*time.Second)
-}
-
-func verifyKillProcess(t *testing.T, namespaceName string, podName string, helmChartReleaseName string, nrReplicasExpected int) {
-	testlib.KillAdminProcess(t, namespaceName, podName)
-	testlib.AwaitNrReplicasScheduled(t, namespaceName, helmChartReleaseName, nrReplicasExpected)
-	testlib.AwaitAdminPodUp(t, namespaceName, podName, 100*time.Second)
-}
-
-func verifyAdminService(t *testing.T, namespaceName string, podName string) {
-	serviceName := "nuodb"
-
-	adminService := testlib.GetService(t, namespaceName, serviceName)
-	assert.Equal(t, adminService.Name, serviceName)
-
-	testlib.PingService(t, namespaceName, serviceName, podName)
-}
 
 func TestKubernetesBasicAdminSingleReplica(t *testing.T) {
 	testlib.AwaitTillerUp(t)
@@ -71,66 +36,6 @@ func TestKubernetesBasicAdminSingleReplica(t *testing.T) {
 	t.Run("verifyPodKill", func(t *testing.T) { verifyPodKill(t, namespaceName, admin0, helmChartReleaseName, 1) })
 	t.Run("verifyProcessKill", func(t *testing.T) { verifyKillProcess(t, namespaceName, admin0, helmChartReleaseName, 1) })
 	t.Run("verifyAdminService", func(t *testing.T) { verifyAdminService(t, namespaceName, admin0) })
-}
-func TestKubernetesBasicAdminThreeReplicas(t *testing.T) {
-	testlib.AwaitTillerUp(t)
-
-	options := helm.Options{
-		SetValues: map[string]string{"admin.replicas": "3"},
-	}
-
-	defer testlib.Teardown(testlib.TEARDOWN_ADMIN)
-
-	helmChartReleaseName, namespaceName := testlib.StartAdmin(t, &options, 3, "")
-
-	admin0 := fmt.Sprintf("%s-nuodb-0", helmChartReleaseName)
-	lbName := fmt.Sprintf("%s-nuodb-balancer", helmChartReleaseName)
-
-	t.Run("verifyAdminState", func(t *testing.T) { testlib.VerifyAdminState(t, namespaceName, admin0) })
-	t.Run("verifyOrderedLicensing", func(t *testing.T) {
-		testlib.VerifyLicenseIsCommunity(t, namespaceName, admin0)
-		testlib.VerifyLicensingErrorsInLog(t, namespaceName, admin0, false) // no error
-	})
-	t.Run("verifyLoadBalancer", func(t *testing.T) { verifyLoadBalancer(t, namespaceName, lbName) })
-	t.Run("verifyLBPolicy", func(t *testing.T) { verifyLBPolicy(t, namespaceName, admin0) })
-	t.Run("verifyPodKill", func(t *testing.T) { verifyPodKill(t, namespaceName, admin0, helmChartReleaseName, 3) })
-	t.Run("verifyProcessKill", func(t *testing.T) { verifyKillProcess(t, namespaceName, admin0, helmChartReleaseName, 3) })
-	t.Run("verifyAdminService", func(t *testing.T) { verifyAdminService(t, namespaceName, admin0) })
-}
-
-func TestKubernetesUpgradeAdmin(t *testing.T) {
-	testlib.AwaitTillerUp(t)
-
-	options := helm.Options{
-		SetValues: map[string]string{"nuodb.image.tag": "4.0"},
-	}
-
-	defer testlib.Teardown(testlib.TEARDOWN_ADMIN)
-
-	helmChartReleaseName, namespaceName := testlib.StartAdmin(t, &options, 1, "")
-
-	admin0 := fmt.Sprintf("%s-nuodb-0", helmChartReleaseName)
-	lbName := fmt.Sprintf("%s-nuodb-balancer", helmChartReleaseName)
-
-	testlib.AwaitBalancerTerminated(t, namespaceName, "job-lb-policy")
-
-	// all jobs need to be deleted before an upgrade can be performed
-	// so far we have not found an automated way to delete them as part of a pre-upgrade hook
-	// if we find it, this line can be removed and the test should still pass
-	testlib.DeletePod(t, namespaceName, "jobs/job-lb-policy-nearest")
-
-	upgradedOptions := &helm.Options{
-		SetValues: map[string]string{"nuodb.image.tag": "4.0.1"},
-	}
-
-	helm.Upgrade(t, upgradedOptions, testlib.ADMIN_HELM_CHART_PATH, helmChartReleaseName)
-
-	testlib.AwaitAdminPodUpgraded(t, namespaceName, admin0, "docker.io/nuodb/nuodb-ce:4.0.1", 300*time.Second)
-	testlib.AwaitAdminPodUp(t, namespaceName, admin0, 300*time.Second)
-
-	t.Run("verifyAdminState", func(t *testing.T) { testlib.VerifyAdminState(t, namespaceName, admin0) })
-	t.Run("verifyLoadBalancer", func(t *testing.T) { verifyLoadBalancer(t, namespaceName, lbName) })
-	t.Run("verifyLBPolicy", func(t *testing.T) { verifyLBPolicy(t, namespaceName, admin0) })
 }
 
 func TestKubernetesInvalidLicense(t *testing.T) {
--- test/minikube/minikube_tls_rotation_test.go
+++ test/minikube/minikube_tls_rotation_test.go
@@ -1,3 +1,5 @@
+// +build long
+
 package minikube
 
 import (
