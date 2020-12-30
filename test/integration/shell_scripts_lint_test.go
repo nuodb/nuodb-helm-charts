5640a381d742bb011401c645d77a9f393c88b22e
Stanimir Ivanov <sivanov@nuodb.com>
2020-12-30 14:31:33 +0200

    Address comments on shell script linting test

--- test/integration/shell_scripts_lint_test.go
+++ test/integration/shell_scripts_lint_test.go
@@ -2,20 +2,20 @@ package integration
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"path"
 	"regexp"
-	"strings"
 	"testing"
 
 	"github.com/nuodb/nuodb-helm-charts/v3/test/testlib"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-const shebangPattern = "[#][!][ \t]*/[^/]+/(sh|bash)"
+const shebangPattern = "[#][!][ \t]*.+[/ ](sh|bash)"
 
-func TestLintScripts(t *testing.T) {
+func TestLintShellScripts(t *testing.T) {
 	rootPaths := [...]string{
 		testlib.ADMIN_HELM_CHART_PATH,
 		testlib.DATABASE_HELM_CHART_PATH,
@@ -25,28 +25,15 @@ func TestLintScripts(t *testing.T) {
 	}
 
 	shebang, err := regexp.Compile(shebangPattern)
-	if err != nil {
-		t.Error("Cannot compile patterh:", shebangPattern)
-	}
+	require.NoError(t, err, "Cannot compile pattern '%s'", shebangPattern)
 
 	first20 := make([]byte, 20)
-
-	var errorList []string
-
-	defer func() {
-		if len(errorList) > 0 {
-			t.Error(strings.Join(errorList, "\nAND "))
-		}
-	}()
 
 	for _, root := range rootPaths {
 		rootPath := path.Join(root, "files")
 
 		info, err := os.Stat(rootPath)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+		require.NoError(t, err)
 
 		if !info.IsDir() {
 			continue
@@ -54,47 +41,36 @@ func TestLintScripts(t *testing.T) {
 
 		// open the path
 		dir, err := os.Open(rootPath)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+		require.NoError(t, err)
+
 		defer dir.Close() // remember to close the File
 
 		// get all files in the dir
 		scripts, err := dir.Readdir(-1)
-		if err != nil {
-			log.Println("Error reading dir", rootPath, ":", err)
-		}
+		require.NoError(t, err, "Error reading dir %s: %s", rootPath, err)
 
 		for _, scriptInfo := range scripts {
 			scriptPath := path.Join(rootPath, scriptInfo.Name())
 
 			script, err := os.Open(scriptPath)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
+			require.NoError(t, err)
+
 			defer script.Close()
 
 			_, err = script.Read(first20)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
+			require.NoError(t, err)
 
 			groups := shebang.FindStringSubmatch(string(first20))
 			if len(groups) == 0 {
-				fmt.Println(scriptPath, " is not a shell script >>", string(first20), "<<")
+				t.Logf("Script %s is not a shell script >>%s<<", scriptPath, string(first20))
 				continue
 			}
 
-			fmt.Println("groups=", groups)
+			t.Logf("Running `%s -n %s`", groups[1], scriptPath)
 			cmd := exec.Command(groups[1], "-n", scriptPath)
 
 			out, err := cmd.CombinedOutput()
-			if err != nil {
-				errorList = append(errorList, fmt.Sprintln(scriptPath, "LINT failed:", err, "\n", string(out)))
-			}
+			assert.NoError(t, err, fmt.Sprintln(scriptPath, "LINT failed:", err, "\n", string(out)))
 		}
 	}
 }
