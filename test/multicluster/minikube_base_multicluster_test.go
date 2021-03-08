b674e6871258a50a36084a654da897b317819942
Stanimir Ivanov <47562317+sivanov-nuodb@users.noreply.github.com>
2021-03-08 16:56:44 +0200

    Multi-cluster support in test framework (#189)
    **Changes**
    - Added CircleCI integration with Azure AKS to support multi-cluster tests
    - Refactored CircleCI config file so that it supports new CircleCI 2.1 workflows and re-usable jobs. As part of this, I've named the workflow jobs and added multi-cluster tests job execution, and added go modules caching so that they are not downloaded every time.
    - Added NuoDB license environment variable to support testing with multiple SMs. It should be masked in all build jobs logs.
    - Enabled some of the tests that require a license to run. This included lowering some of the specs for the engines so that they fit in the local minikube installations.
    - Added multi-cluster machinery in the `testlib` which allows to easily install multi-cluster environments using already available test infrastructure.
    - Bumped up some of the timeouts as AKS seems a bit slower in scheduling pods (maybe due to the dynamic PV provisioning)
    - Added more diagnostics in case pods are not schedules in the desired timeouts
    
    **Testing**
    - Implemented one basic multi-cluster test in a separate test suite
    - Regression testing

---
+++ test/multicluster/minikube_base_multicluster_test.go
@@ -0,0 +1,108 @@
+package multicluster
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/nuodb/nuodb-helm-charts/v3/test/testlib"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/random"
+)
+
+func verifyNuoSQL(t *testing.T, namespaceName string, adminPod string, databaseName string) {
+	output, err := testlib.RunSQL(t, namespaceName, adminPod, databaseName, "select * from system.nodes")
+	require.NoError(t, err, output)
+	require.True(t, strings.Contains(output, "Storage"))
+	require.True(t, strings.Contains(output, "Transaction"))
+}
+
+func TestKubernetesBasicMultiCluster(t *testing.T) {
+	if os.Getenv("NUODB_LICENSE") != "ENTERPRISE" && os.Getenv("NUODB_LICENSE_CONTENT") == "" {
+		t.Skip("Cannot test multiple SMs without the Enterprise Edition")
+	}
+	testlib.AwaitTillerUp(t)
+	defer testlib.VerifyTeardown(t)
+
+	// For multi-cluster deployment to work correctly, there are two prerequisites:
+	// - the same namespace name should be used in all clusters
+	// - the same admin Helm release should be used in all cluster (probably something to fix)
+	randomSuffix := strings.ToLower(random.UniqueId())
+	adminReleaseName := fmt.Sprintf("admin-%s", randomSuffix)
+	namespaceName := fmt.Sprintf("kubernetesbasicmulticluster-%s", randomSuffix)
+
+	options := helm.Options{
+		SetValues: map[string]string{
+			"database.sm.resources.requests.cpu":    testlib.MINIMAL_VIABLE_ENGINE_CPU,
+			"database.sm.resources.requests.memory": testlib.MINIMAL_VIABLE_ENGINE_MEMORY,
+			"database.te.resources.requests.cpu":    testlib.MINIMAL_VIABLE_ENGINE_CPU,
+			"database.te.resources.requests.memory": testlib.MINIMAL_VIABLE_ENGINE_MEMORY,
+		},
+	}
+
+	context := context.Background()
+	cluster1Context := testlib.NewClusterDeploymentContext(context,
+		&options, testlib.MULTI_CLUSTER_1, testlib.MULTI_CLUSTER_1)
+	cluster2Context := testlib.NewClusterDeploymentContext(context,
+		&options, testlib.MULTI_CLUSTER_2, testlib.MULTI_CLUSTER_1)
+
+	defer testlib.Teardown(testlib.TEARDOWN_MULTICLUSTER)
+	defer testlib.Teardown(testlib.TEARDOWN_ADMIN)
+	defer testlib.Teardown(testlib.TEARDOWN_DATABASE)
+
+	testlib.DeployWithContext(t,
+		cluster1Context,
+		func(context *testlib.ClusterDeploymentContext, options *helm.Options) {
+			testlib.CreateNamespace(t, namespaceName)
+			testlib.StartAdminCustomRelease(t, options, 1, namespaceName, adminReleaseName)
+			admin0 := fmt.Sprintf("%s-nuodb-%s-0", adminReleaseName, context.ThisCluster.Name)
+			testlib.ApplyNuoDBLicense(t, namespaceName, admin0)
+			databaseReleaseName := testlib.StartDatabase(t, namespaceName, admin0, options)
+			// Store deployment details in the context
+			context.AdminReleaseName = adminReleaseName
+			context.DatabaseReleaseName = databaseReleaseName
+			context.Namespace = namespaceName
+		},
+	)
+
+	testlib.DeployWithContext(t,
+		cluster2Context,
+		func(context *testlib.ClusterDeploymentContext, options *helm.Options) {
+			testlib.CreateNamespace(t, namespaceName)
+			testlib.StartAdminCustomRelease(t, options, 1, namespaceName, adminReleaseName)
+			admin0 := fmt.Sprintf("%s-nuodb-%s-0", adminReleaseName, context.ThisCluster.Name)
+			databaseReleaseName := testlib.StartDatabase(t, namespaceName, admin0, options)
+			// Store deployment details in the context
+			context.AdminReleaseName = adminReleaseName
+			context.DatabaseReleaseName = databaseReleaseName
+			context.Namespace = namespaceName
+		},
+	)
+
+	// Generate diagnose in case this test fails
+	testlib.AddDiagnosticTeardown(testlib.TEARDOWN_DATABASE, t, func() {
+		testlib.ExecuteInAllClusters(t, func(context *testlib.ClusterDeploymentContext) {
+			admin0 := fmt.Sprintf("%s-nuodb-%s-0", context.AdminReleaseName, context.ThisCluster.Name)
+			testlib.GetDiagnoseOnTestFailure(t, context.Namespace, admin0)
+		})
+	})
+
+	t.Run("verifyDomain", func(t *testing.T) {
+		testlib.ExecuteInAllClusters(t, func(context *testlib.ClusterDeploymentContext) {
+			admin0 := fmt.Sprintf("%s-nuodb-%s-0", context.AdminReleaseName, context.ThisCluster.Name)
+			testlib.AwaitAdminFullyConnected(t, context.Namespace, admin0, 2)
+			testlib.AwaitDatabaseUp(t, context.Namespace, admin0, "demo", 4)
+		})
+	})
+	t.Run("verifyNuoSQL", func(t *testing.T) {
+		testlib.ExecuteInAllClusters(t, func(context *testlib.ClusterDeploymentContext) {
+			admin0 := fmt.Sprintf("%s-nuodb-%s-0", context.AdminReleaseName, context.ThisCluster.Name)
+			verifyNuoSQL(t, context.Namespace, admin0, "demo")
+		})
+	})
+}
