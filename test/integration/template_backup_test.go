18f9b5a4185d7e86f867737d01fa291882461d37
Stanimir Ivanov <47562317+sivanov-nuodb@users.noreply.github.com>
2021-04-14 14:49:43 +0300

    Change the default timeout for all backup jobs (#197)
    - Default timeout for full, incremental and journal backup jobs has been changed to 0 which means that they should wait forever for the underlying hot-copy operation to complete.
    - Journal backup timeout is not used when invoking nuobackup
    - Make nuobackup non-blocking. Similar to our other commands, the user needs to specify --timeout if he wants it to block synchronously.

--- test/integration/template_backup_test.go
+++ test/integration/template_backup_test.go
@@ -1,11 +1,14 @@
 package integration
 
 import (
+	"strings"
+	"testing"
+
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/nuodb/nuodb-helm-charts/v3/test/testlib"
-	"gotest.tools/assert"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
-	"testing"
 )
 
 func TestDatabaseBackupCronJobRenders(t *testing.T) {
@@ -35,7 +38,7 @@ func TestDatabaseBackupCronJobDisabled(t *testing.T) {
 	// Run RenderTemplate to render the template and capture the output.
 	_, err := helm.RenderTemplateE(t, options, helmChartPath, "release-name", []string{"templates/cronjob.yaml"})
 
-	assert.ErrorContains(t, err, "could not find template")
+	assert.Contains(t, err.Error(), "could not find template")
 }
 
 func TestDatabaseBackupCronJobRestartPolicyDefault(t *testing.T) {
@@ -69,5 +72,57 @@ func TestDatabaseBackupCronJobRestartPolicyOverride(t *testing.T) {
 
 	for _, job := range testlib.SplitAndRenderCronJob(t, output, 2) {
 		assert.Equal(t, job.Spec.JobTemplate.Spec.Template.Spec.RestartPolicy, v1.RestartPolicyNever)
+	}
+}
+
+func TestDatabaseBackupTimeout(t *testing.T) {
+	// Path to the helm chart we will test
+	helmChartPath := "../../stable/database"
+
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"database.sm.hotCopy.journalBackup.enabled": "true",
+		},
+	}
+
+	// Run RenderTemplate to render the template and capture the output.
+	output := helm.RenderTemplate(t, options, helmChartPath, "release-name", []string{"templates/cronjob.yaml"})
+
+	// verify that default value for timeout is rendered
+	for _, obj := range testlib.SplitAndRenderCronJob(t, output, 2) {
+		require.NotEmpty(t, obj.Spec.JobTemplate.Spec.Template.Spec.Containers)
+		backupContainer := obj.Spec.JobTemplate.Spec.Template.Spec.Containers[0]
+		assert.Subset(t, backupContainer.Args, []string{
+			"--timeout",
+			"0",
+		})
+	}
+
+	options = &helm.Options{
+		SetValues: map[string]string{
+			"database.sm.hotCopy.journalBackup.enabled": "true",
+			"database.sm.hotCopy.timeout":               "1800",
+			"database.sm.hotCopy.journalBackup.timeout": "950",
+		},
+	}
+
+	// Run RenderTemplate to render the template and capture the output.
+	output = helm.RenderTemplate(t, options, helmChartPath, "release-name", []string{"templates/cronjob.yaml"})
+
+	// verify that configured value for timeout is rendered
+	for _, obj := range testlib.SplitAndRenderCronJob(t, output, 2) {
+		require.NotEmpty(t, obj.Spec.JobTemplate.Spec.Template.Spec.Containers)
+		backupContainer := obj.Spec.JobTemplate.Spec.Template.Spec.Containers[0]
+		if strings.Contains(obj.Name, "journal") {
+			assert.Subset(t, backupContainer.Args, []string{
+				"--timeout",
+				"950",
+			})
+		} else {
+			assert.Subset(t, backupContainer.Args, []string{
+				"--timeout",
+				"1800",
+			})
+		}
 	}
 }
