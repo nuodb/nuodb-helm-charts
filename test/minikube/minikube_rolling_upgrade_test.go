55685d3deffe425b7e0c0ead9b39a4c69ab0915f
Stanimir Ivanov <47562317+sivanov-nuodb@users.noreply.github.com>
2021-09-28 20:33:45 +0300

    Upgrade database protocol automatically (#243)
    Starting from NuoDB 4.2.3, the Kubernetes Aware Admin (KAA) can upgrade the database protocol version automatically (turned off by default).
    Expose this functionality in NuoDB Helm Charts so that NuoDB product upgrade process can be easily automated.

--- test/minikube/minikube_rolling_upgrade_test.go
+++ test/minikube/minikube_rolling_upgrade_test.go
@@ -4,15 +4,20 @@ package minikube
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 	"testing"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/nuodb/nuodb-helm-charts/v3/test/testlib"
+
+	"github.com/Masterminds/semver"
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/shell"
-	"github.com/nuodb/nuodb-helm-charts/v3/test/testlib"
 	"github.com/stretchr/testify/require"
 
 	v12 "k8s.io/api/core/v1"
@@ -153,7 +158,7 @@ func TestKubernetesUpgradeAdminMinorVersion(t *testing.T) {
 	t.Run("verifyAdminState", func(t *testing.T) { testlib.VerifyAdminState(t, namespaceName, admin0) })
 }
 
-func TestKubernetesUpgradeFullDatabaseMinorVersion(t *testing.T) {
+func TestKubernetesUpgradeFullDatabase(t *testing.T) {
 	testlib.AwaitTillerUp(t)
 	defer testlib.VerifyTeardown(t)
 
@@ -172,7 +177,9 @@ func TestKubernetesUpgradeFullDatabaseMinorVersion(t *testing.T) {
 	testlib.OverrideUpgradeContainerImage(t, &options)
 
 	randomSuffix := strings.ToLower(random.UniqueId())
-	namespaceName := fmt.Sprintf("upgradefulldatabaseminorversion-%s", randomSuffix)
+	namespaceName := fmt.Sprintf("upgradefulldatabase-%s", randomSuffix)
+	kubectlOptions := k8s.NewKubectlOptions("", "", namespaceName)
+	options.KubectlOptions = kubectlOptions
 	testlib.CreateNamespace(t, namespaceName)
 
 	// Enable TLS during upgrade because NuoDB 4.2+ doesn't include
@@ -232,6 +239,72 @@ func TestKubernetesUpgradeFullDatabaseMinorVersion(t *testing.T) {
 
 		verifyAllProcessesRunning(t, namespaceName, admin0, 2)
 	})
+
+	testlib.RunOnNuoDBVersionCondition(t, ">4.2.2", func(version *semver.Version) {
+		// check that KAA will upgrade database protocol version and restart TE
+		// automatically
+		t.Run("verifyProtocolVersion", func(t *testing.T) {
+			// enable automatic protocol upgrade
+			options.SetValues["database.automaticProtocolUpgrade.enabled"] = "true"
+			helm.Upgrade(t, &options, testlib.DATABASE_HELM_CHART_PATH, databaseHelmChartReleaseName)
+
+			// protocol upgrade is async task
+			testlib.Await(t, func() bool {
+				return testlib.GetStringOccurrenceInLog(t, namespaceName, admin0,
+					fmt.Sprintf("Upgrading protocol version for database dbName=%s", opt.DbName),
+					&corev1.PodLogOptions{}) >= 1
+			}, 60*time.Second)
+
+			// fetch database effective version
+			output, err := k8s.RunKubectlAndGetOutputE(t, options.KubectlOptions, "exec", admin0, "--",
+				"nuocmd", "show", "database-versions", "--db-name", opt.DbName)
+			require.NoError(t, err, "running show database-versions failed")
+			pattern := regexp.MustCompile("effective version ID: ([0-9]+), effective version: (.+),")
+			match := pattern.FindStringSubmatch(output)
+			require.NotNil(t, match, "Unable to get database effective version from output")
+
+			// effective version is string like this one 4.2|4.2.1|4.2.2; verify
+			// that the OLD_RELEASE major.minor is not in the effective version
+			// string
+			parts := strings.Split(OLD_RELEASE, ".")
+			require.GreaterOrEqual(t, len(parts), 2, "unable to get major.minor from OLD_RELEASE version")
+			require.NotContains(t, match[2], fmt.Sprintf("%s.%s", parts[0], parts[1]))
+			// verify that the new major.minor is in the effective version
+			// string
+			require.Contains(t, match[2], fmt.Sprintf("%d.%d", version.Major(), version.Minor()))
+
+			// verify that a TE has been restarted and SQL layer version is
+			// upgrade is performed
+			testlib.Await(t, func() bool {
+				return testlib.GetRegexOccurrenceInLog(t, namespaceName, admin0,
+					"Shutting down startId=[0-9]+ to finalize the database protocol upgrade",
+					&corev1.PodLogOptions{}) >= 1
+			}, 60*time.Second)
+			podName := testlib.GetPodName(t, namespaceName,
+				fmt.Sprintf("te-%s-nuodb-cluster0-%s", databaseHelmChartReleaseName, opt.DbName))
+			testlib.AwaitPodRestartCountGreaterThan(t, namespaceName, podName, 0, 30*time.Second)
+			testlib.AwaitDatabaseUp(t, namespaceName, admin0, opt.DbName, 2)
+			output, err = testlib.RunSQL(t, namespaceName, admin0, opt.DbName,
+				"select version from system.versions"+
+					" where property = 'SYSTEM_TABLES_VERSION'"+
+					" and version = GETEFFECTIVEPLATFORMVERSION()")
+			require.NoError(t, err, "error cheking if SQL layer version is upgraded")
+			require.Contains(t, output, match[1])
+
+			// verify that the container image ID is stored in KV store so that
+			// database version is not checked again for this container image
+			// ID; this happens on the next resync once database versions
+			// response is inspected
+			testlib.Await(t, func() bool {
+				adminPod := testlib.GetPod(t, namespaceName, admin0)
+				actualImageId, err := k8s.RunKubectlAndGetOutputE(t, options.KubectlOptions, "exec", admin0, "--",
+					"nuocmd", "get", "value", "--key", fmt.Sprintf("upgradeDatabaseLastObservedImage/%s", opt.DbName))
+				require.NoError(t, err, "error getting last observed image ID")
+				return adminPod.Status.ContainerStatuses[0].ImageID == actualImageId
+			}, 60*time.Second)
+		})
+	})
+
 }
 
 func TestKubernetesRollingUpgradeAdminMinorVersion(t *testing.T) {
