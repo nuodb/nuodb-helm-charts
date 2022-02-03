9bb24f8157156fecbaf1bad3d062e58ff5cfc7ef
Stanimir Ivanov <sivanov@nuodb.com>
2022-02-03 08:53:58 +0200

    Address review comments round 2

--- test/integration/template_admin_test.go
+++ test/integration/template_admin_test.go
@@ -844,6 +844,18 @@ func TestAdminServiceAccount(t *testing.T) {
 		for _, obj := range testlib.SplitAndRenderStatefulSet(t, output, 1) {
 			assert.Empty(t, obj.Spec.Template.Spec.ServiceAccountName)
 		}
+
+		options = &helm.Options{
+			SetValues: map[string]string{
+				"nuodb.addServiceAccount": "true",
+				"nuodb.serviceAccount":    "",
+			},
+		}
+
+		// nuodb ServiceAccount is not created
+		_, err = helm.RenderTemplateE(t, options, helmChartPath, "release-name", []string{"templates/serviceaccount.yaml"})
+		assert.NotNil(t, err)
+		assert.Contains(t, err.Error(), "could not find template templates/serviceaccount.yaml in chart")
 	})
 
 }
