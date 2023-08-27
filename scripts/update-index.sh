27dbb28b415d2e76cce885bf35534b2588385c43
Adrian Suarez <asz6@3ds.com>
2023-08-26 20:18:06 -0400

    Add update-index.sh script

---
+++ scripts/update-index.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+fail() {
+    printf "$1\n" >&2
+    exit 1
+}
+
+set -e
+
+# Make sure there are no uncommitted changes
+GIT_STATUS="$(git status --porcelain)"
+[ "$GIT_STATUS" = "" ] || fail "Cannot publish charts with uncommitted changes:\n$GIT_STATUS"
+
+# Change to root directory and make sure package directory exists
+cd "$(dirname "$0")/.."
+[ -d package ] || fail "package directory does not exist... run package.sh first"
+
+# Checkout gh-pages and fast forward to origin
+git checkout gh-pages
+git merge --ff-only origin/gh-pages
+
+# Update index with new Helm charts
+: ${GH_RELEASES_URL:="https://github.com/nuodb/nuodb-helm-charts/releases/download"}
+helm repo index package --merge index.yaml --url "$GH_RELEASES_URL"
