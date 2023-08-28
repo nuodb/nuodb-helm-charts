e163778856675476833c5ce45e44b4ef79f77840
Adrian Suarez <asz6@3ds.com>
2023-08-28 12:45:16 -0400

    Automate publishing of incubator charts
    This change updates the package.sh and update-index.sh scripts to also
    create incubator Helm charts, attach them to the created GitHub release,
    and add them to the gh-pages:incubator/index.yaml file.
    
    This change assumes a change to the gh-pages branch to structure index
    files as follows:
    
    ```
    index.yaml -> stable/index.yaml (symlink)
    stable/
    - index.yaml
    incubator/
    - index.yaml
    ```

--- scripts/update-index.sh
+++ scripts/update-index.sh
@@ -5,28 +5,43 @@ fail() {
     exit 1
 }
 
+check_package() {
+    [ -d "package/$1" ] || fail "package/$1 directory does not exist... run package.sh first"
+}
+
+update_index() {
+    # Merge new charts into current index
+    : ${GH_RELEASES_URL:="https://github.com/nuodb/nuodb-helm-charts/releases/download"}
+    helm repo index "package/$1" --merge "$1/index.yaml" --url "$GH_RELEASES_URL"
+
+    # Stage update to index file
+    mv "package/$1/index.yaml" "$1/index.yaml"
+    git add "$1/index.yaml"
+}
+
 set -e
 
 # Make sure there are no uncommitted changes
 GIT_STATUS="$(git status --porcelain)"
 [ "$GIT_STATUS" = "" ] || fail "Cannot publish charts with uncommitted changes:\n$GIT_STATUS"
 
-# Change to root directory and make sure package directory exists
+# Change to root directory and make sure package directories exist
 cd "$(dirname "$0")/.."
-[ -d package ] || fail "package directory does not exist... run package.sh first"
+check_package stable
+check_package incubator
 
 # Checkout gh-pages and fast forward to origin
 git checkout gh-pages
 git merge --ff-only origin/gh-pages
 
-# Update index with new Helm charts
-: ${GH_RELEASES_URL:="https://github.com/nuodb/nuodb-helm-charts/releases/download"}
-helm repo index package --merge index.yaml --url "$GH_RELEASES_URL"
+# Update indexes with new Helm charts
+update_index stable
+update_index incubator
 
-# Commit and push change if PUSH_UPDATE=true
+# Commit changes to indexes
+git commit -m "Add $(ls package/stable | sed 's|/||') charts to indexes"
+
+# Push changes if PUSH_UPDATE=true
 if [ "$PUSH_UPDATE" = true ]; then
-    mv package/index.yaml .
-    git add index.yaml
-    git commit -m "Adding $(ls package | sed 's|/||') charts to index"
     git push
 fi
