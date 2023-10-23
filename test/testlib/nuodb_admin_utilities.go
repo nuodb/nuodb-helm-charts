1e0992a52e7a4f1e87df183c2ab982bc44830b34
AGARWAL Harsh Mohan (INTERN) <HAL5@dsone.3ds.com>
2023-10-23 11:16:38 -0400

    Style of comment changed

--- test/testlib/nuodb_admin_utilities.go
+++ test/testlib/nuodb_admin_utilities.go
@@ -143,12 +143,12 @@ func StartAdminTemplate(t *testing.T, options *helm.Options, replicaCount int, n
 		}
 	}
 
-	/** 
-		LIMITED license takes priority over Enterprise license because
-	 	we don't want to change the test flow for NuoDB version 6.0
-	 	Any test which requires Enterprise license would install it using
-	 	ApplyLicense
-	**/	
+	
+	// LIMITED license takes priority over Enterprise license because
+	// we don't want to change the test flow for NuoDB version 6.0.
+	// Any test which requires Enterprise license would install it using
+	// ApplyLicense function
+	
 
 	if os.Getenv("NUODB_LIMITED_LICENSE_CONTENT") != "" {
 		ApplyLicense(t, namespaceName, adminNames[0], LIMITED)
