c7b3e1c5381ed6cab72a240a5e06b34838c4a0dd
Stanimir Ivanov <47562317+sivanov-nuodb@users.noreply.github.com>
2023-11-06 10:35:19 +0200

    Supply NuoDB license declaratively via secret (#336)
    Mount NuoDB license declaratively using Secret resource.
    This requires NuoDB image v5.1.1 and above.

--- test/minikube/minikube_base_admin_test.go
+++ test/minikube/minikube_base_admin_test.go
@@ -4,11 +4,21 @@
 package minikube
 
 import (
+	"context"
+	"encoding/base64"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"github.com/nuodb/nuodb-helm-charts/v3/test/testlib"
 )
 
@@ -16,7 +26,7 @@ func TestKubernetesBasicAdminSingleReplica(t *testing.T) {
 	testlib.AwaitTillerUp(t)
 	defer testlib.VerifyTeardown(t)
 
-	options := helm.Options {
+	options := helm.Options{
 		SetValues: map[string]string{},
 	}
 
@@ -48,6 +58,66 @@ func TestKubernetesBasicAdminSingleReplica(t *testing.T) {
 		t.Skip("verifyPodKill is flaky")
 		verifyPodKill(t, namespaceName, admin0, helmChartReleaseName, 1)
 	})
+}
+
+func TestKubernetesAdminLicenseSecret(t *testing.T) {
+	testlib.SkipTestOnNuoDBVersionCondition(t, "< 5.1.1")
+	if os.Getenv("NUODB_LICENSE_CONTENT") == "" {
+		t.Skip("Cannot run this test without a valid license")
+	}
+	testlib.AwaitTillerUp(t)
+	defer testlib.VerifyTeardown(t)
+
+	randomSuffix := strings.ToLower(random.UniqueId())
+	namespaceName := fmt.Sprintf("%skubernetesadminlicensesecret-%s", testlib.NAMESPACE_NAME_PREFIX, randomSuffix)
+	testlib.CreateNamespace(t, namespaceName)
+
+	ctx := context.Background()
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "nuodb-license",
+			Namespace: namespaceName,
+		},
+		StringData: map[string]string{
+			"nuodb.lic": "garbage",
+		},
+	}
+
+	options := helm.Options{
+		SetValues: map[string]string{
+			"admin.license.secret": "nuodb-license",
+		},
+	}
+	kubectlOptions := k8s.NewKubectlOptions("", "", namespaceName)
+
+	clientset, err := k8s.GetKubernetesClientFromOptionsE(t, kubectlOptions)
+	require.NoError(t, err)
+	clientset.CoreV1().Secrets(namespaceName).Create(ctx, secret, metav1.CreateOptions{})
+
+	defer testlib.Teardown(testlib.TEARDOWN_ADMIN)
+
+	helmChartReleaseName, _ := testlib.StartAdmin(t, &options, 1, namespaceName)
+
+	admin0 := fmt.Sprintf("%s-nuodb-cluster0-0", helmChartReleaseName)
+	testlib.VerifyLicense(t, namespaceName, admin0, testlib.UNLICENSED)
+	testlib.VerifyLicensingErrorsInLog(t, namespaceName, admin0, true)
+
+	// Update the secret with the NuoDB EE license
+	licenseContentBytes, err := base64.StdEncoding.DecodeString(os.Getenv("NUODB_LICENSE_CONTENT"))
+	require.NoError(t, err)
+	secret, err = clientset.CoreV1().Secrets(namespaceName).Get(ctx, secret.Name, metav1.GetOptions{})
+	require.NoError(t, err)
+	secret.Data["nuodb.lic"] = licenseContentBytes
+	secret, err = clientset.CoreV1().Secrets(namespaceName).Update(ctx, secret, metav1.UpdateOptions{})
+	require.NoError(t, err)
+
+	t.Log("Waiting for license update")
+	testlib.Await(t, func() bool {
+		if err := testlib.VerifyLicenseE(t, namespaceName, admin0, testlib.ENTERPRISE); err != nil {
+			return false
+		}
+		return true
+	}, 180*time.Second)
 }
 
 func TestKubernetesInvalidLicense(t *testing.T) {
