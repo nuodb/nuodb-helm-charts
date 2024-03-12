05a3aedc8108ded8b7ed0d1bb4f4a6def451c02a
Adrian Suarez <asz6@3ds.com>
2024-03-12 19:58:17 -0400

    Use latest nuosql, which supports SNI

--- scripts/ci/install_deps.sh
+++ scripts/ci/install_deps.sh
@@ -149,6 +149,6 @@ fi
 curl -sSL "https://github.com/gotestyourself/gotestsum/releases/download/v"${GOTESTSUM_VERSION}"/gotestsum_${GOTESTSUM_VERSION}_linux_amd64.tar.gz" | sudo tar -xz -C /usr/local/bin gotestsum
 
 # Install NuoDB client on the build host
-curl -sSL "https://github.com/nuodb/nuodb-client/releases/download/v${NUODBCLIENT_VERSION}/nuodb-client-${NUODBCLIENT_VERSION}.lin64.tar.gz" | sudo tar -xz -C $HOME
-echo "export PATH=${HOME}/nuodb-client-${NUODBCLIENT_VERSION}.lin64/bin:\$PATH" >> $HOME/.nuodbrc
+curl -sSL "https://github.com/nuodb/nuodb-client/releases/download/v${NUODBCLIENT_VERSION}/nuodb-client-${NUODBCLIENT_VERSION}.lin-x64.tar.gz" | sudo tar -xz -C $HOME
+echo "export PATH=${HOME}/nuodb-client-${NUODBCLIENT_VERSION}.lin-x64/bin:\$PATH" >> $HOME/.nuodbrc
 echo "echo PATH: \"\$PATH\"" >> $HOME/.nuodbrc
